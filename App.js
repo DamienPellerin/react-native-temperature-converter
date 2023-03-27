2d459cf4a23b755e5e00fad12be6886836bc879f
Damien Pellerin <damienpellerin19@gmail.com>
2023-03-27 16:10:18 +0200

    v1

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
+import { s } from "./App.style";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={s.container}>
+      <View style={s.workspace}>
+        <View>
+          <Text>Temperature</Text>
+        </View>
+        <View>
+          <Text>Input</Text>
+        </View>
+        <View>
+          <Text>Button</Text>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
